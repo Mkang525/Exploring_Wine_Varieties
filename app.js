b35b1768563400ec8252b7d72fd243e15cb12773
Emma Limoli <EmmaLimoli024@gmail.com>
2020-10-09 12:08:51 -0400

    started working on bubble chart

--- app.js
+++ app.js
@@ -10,21 +10,72 @@ function wineData(variety) {
     d3.csv("practice_dataset.csv").then(function(data) {
         console.log(data);
 
+        // create varieties for y axis
         var wineType = data.map(w => w.variety)
         console.log(wineType)
-        // var wineType = data.filter(w => w.variety)
-        console.log("Variety Of Wine: ", wineType);
-        // var wineSlices = wineType
-        
+        var wineType2 = wineType.filter(w => {return w.variety == variety})
+        console.log(wineType2);
+        wineFinal = wineType2;
+        console.log("Variety Of Wine: ", wineFinal);
+        var wineSlices = wineFinal.slice(0, 5);
+        console.log(wineSlices)
+
+        // create varieties for pricing
+        var winePrice = data.map(p => p.price)
+        console.log(winePrice)
+        // var winePrice = data.filter(p => {return p.price == price})
+        // console.log(winePrice);
+        // winePrice = winePrice;
+        console.log("Wine Prices: ", winePrice);
+        var priceSlices = winePrice.slice(0, 5);
+        console.log(priceSlices)
+
+        // create labels for hover over function
+        var labels = data.map(t => t.title)
+        console.log(labels)
+        var labelsSlices = labels.slice(0,5);
+        console.log(labelsSlices)
+
+
+// bubble chart
+// where to put the conditional to change the sizing of the bubbles 
+// based on pricing
+var trace = {
+    x: priceSlices,
+    y: wineSlices,
+    text: labelsSlices,
+    mode: 'markers',
+    marker: {
+        size: 50,
+        symbol: 'diamond',
+        sizeref: 1000,
+        opacity: [0.5],
+        color: 'red',
+    }
+};
+
+var data = [trace];
+
+var layout = {
+    xaxis: {title: "Pricing"},
+    title: "Pricing For Wine Varieties",
+    height: 400,
+    width: 800,
+    margin: {
+        l: 100
+    }
+};
+
+Plotly.newPlot("bubble", data, layout);
     })
 };
 wineData();
 
-// bubble chart
-
+// link to HTML optionChanged
 function optionChanged(variety) {
     wineData(variety);
 }
+// function for dropdown
 // need to write a condition to put different wines into groups
 function init() {
     var dropdownMenu = d3.select("#selDataset");
