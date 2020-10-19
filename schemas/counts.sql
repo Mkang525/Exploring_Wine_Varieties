5fdaa6e3e10924ed3c4efefa195e8b2e22c089c6
EmmaAB <66276192+EmmaAB@users.noreply.github.com>
2020-10-18 19:29:17 -0700

    Update counts.sql

--- schemas/counts.sql
+++ schemas/counts.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS winecounts;
+DROP TABLE IF EXISTS topten;
 
 --Create a table for wine reviews
 CREATE TABLE winecounts (
@@ -15,3 +16,19 @@ Counts INTEGER
 );
 	
 SELECT * FROM winecounts;
+
+--Create a table for wine reviews
+CREATE TABLE topten (
+ID INT NOT NULL PRIMARY KEY,
+Title VARCHAR,
+Country VARCHAR,
+Points INTEGER,
+Price VARCHAR,
+Province VARCHAR,
+Variety VARCHAR,
+Winery VARCHAR,
+Lat FLOAT,
+Lng FLOAT
+);
+
+SELECT * FROM topten
