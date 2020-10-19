be5803b8bd732ece4bbd34cd103d3cad78a7ea04
EmmaAB <66276192+EmmaAB@users.noreply.github.com>
2020-10-19 09:00:29 -0700

    Update topten.sql

--- schemas/topten.sql
+++ schemas/topten.sql
@@ -1,4 +1,21 @@
+DROP TABLE IF EXISTS winecounts;
 DROP TABLE IF EXISTS topten;
+
+--Create a table for wine reviews
+CREATE TABLE winecounts (
+ID VARCHAR PRIMARY KEY,
+Title VARCHAR,
+Country VARCHAR,
+Points INTEGER,
+Price FLOAT,
+Province VARCHAR,
+Variety VARCHAR,
+Winery VARCHAR,
+Count INTEGER,
+Counts INTEGER
+);
+	
+SELECT * FROM winecounts;
 
 --Create a table for wine reviews
 CREATE TABLE topten (
@@ -15,4 +32,3 @@ Lng FLOAT
 );
 
 SELECT * FROM topten
-
