04a14f1bc23e2ef2eb1b566b9609f2af1e29615f
EmmaAB <66276192+EmmaAB@users.noreply.github.com>
2020-10-19 08:41:45 -0700

    Add files via upload

---
+++ UPDATED_APP/static/js/data.js
@@ -0,0 +1,252 @@
+var wine_descr_data =[
+    {
+        "Variety": "Pinot Noir",
+        "Type": "Red",
+        "Descriptions": "Pinot noir is a type of wine grape and a style of red wine that is typically light to medium-bodied, fruit-forward, and relatively low alcohol compared to other red wines. Originally from Burgundy, the pinot noir grape is notoriously hard to grow. Known as \"red Burgundy\" in France, the grapes are now grown under favorable conditions all over the world."
+      },
+      {
+        "Variety": "Chardonnay",
+        "Type": "White",
+        "Descriptions": "Typically, Chardonnay is a dry, medium- to full-bodied wine with moderate acidity and alcohol. Its flavors range from apple and lemon to papaya and pineapple, and it also shows notes of vanilla when it's aged with oak."
+      },
+      {
+        "Variety": "Cabernet Sauvignon",
+        "Type": "Red",
+        "Descriptions": "Cabernet Sauvignon is well known for its firm tannins and full and rich taste, including such notes as blackcurrant, as well as woodsy flavors, such as cedar, oak, and herbs. The wine also contains great potential for aging, thanks to its high rate of tannins and perceptible acidity."
+      },
+      {
+        "Variety": "Red Blend",
+        "Type": "Red",
+        "Descriptions": "Red blend is a label designation for red wines that contain more than one grape variety in the final cuvée. These wines can be made following well-loved traditional formulas, but this category contains a wide range of combinations. Red blend wines vary in color, aroma, flavor, structure and ageability."
+      },
+      {
+        "Variety": "Bordeaux-style Red Blend",
+        "Type": "Red",
+        "Descriptions": "The phrase “Bordeaux-style red blend” may be used informally to describe red wines produced from a combination of Cabernet Sauvignon, Merlot, Cabernet Franc, Petit Verdot, and to a lesser extent Carmenère and Malbec."
+      },
+      {
+        "Variety": "Sauvignon Blanc",
+        "Type": "White",
+        "Descriptions": "Sauvignon Blanc is a white wine grape variety that is most often used to produce dry white wines. The flavor can range from aggressively grassy to sweetly tropical."
+      },
+      {
+        "Variety": "Riesling",
+        "Type": "White",
+        "Descriptions": "Riesling is an aromatic grape variety displaying flowery, almost perfumed, aromas as well as high acidity. It is used to make dry, semi-sweet, sweet, and sparkling white wines. Riesling wines are usually varietally pure and are seldom oaked."
+      },
+      {
+        "Variety": "Syrah",
+        "Type": "Red",
+        "Descriptions": "The style and flavor profile of Syrah, also known as Shiraz, are influenced by the climate where the grapes are grown. It ranges from medium to full-bodied wines with notes of blackberry, mint and black pepper to a more full-bodied with softer tannin, jammier fruit and spice notes of licorice, anise and earthy leather."
+      },
+      {
+        "Variety": "Rosé",
+        "Type": "Rosé",
+        "Descriptions": "A rosé is a type of wine that incorporates some of the color from the grape skins, but not enough to qualify it as a red wine. Rosé wines can be made still, semi-sparkling or sparkling and with a wide range of sweetness levels."
+      },
+      {
+        "Variety": "Merlot",
+        "Type": "Red",
+        "Descriptions": "Typically, Merlot is a dry, medium- to full-bodied wine with moderate acidity, moderate to high alcohol, and soft but present tannins."
+      },
+      {
+        "Variety": "Malbec",
+        "Type": "Red",
+        "Descriptions": "Malbec  is a purple grape variety used in making red wine. The grapes tend to have an inky dark color and robust tannins."
+      },
+      {
+        "Variety": "Nebbiolo",
+        "Type": "Red",
+        "Descriptions": "Nebbiolo is a full-bodied red wine more famously known by the two production regions of Barolo and Barbaresco in Piedmont, Italy. Nebbiolo wines are translucent  and have a delicate smell, but robust tannin and high acidity."
+      },
+      {
+        "Variety": "Sangiovese",
+        "Type": "Red",
+        "Descriptions": "Sangiovese is a dry, light to medium-bodied red wine that tips towards higher levels of mouth-watering acidity and tighter tannins. The rich flavors range from rustic to fruity, depending on where and how the vines are managed. For fruit, expect cherry, plum, and red currant, as well as smoky and earthy herbaceousness."
+      },
+      {
+        "Variety": "Zinfandel",
+        "Type": "Red",
+        "Descriptions": "Zinfandel (also known as Primitivo) is a variety of black-skinned wine grape. The grapes typically produce a robust red wine, with a high sugar content that can be fermented into levels of alcohol exceeding 15 percent."
+      },
+      {
+        "Variety": "Portuguese Red",
+        "Type": "Red",
+        "Descriptions": "Portugal's red wines are often bold with firm structure and dark fruit flavor. They are made from both indigenous grapes like Touriga Nacional and international varieties like Merlot."
+      },
+      {
+        "Variety": "White Blend",
+        "Type": "White",
+        "Descriptions": "“White blend” refers to any white wine that contains more than one white grape varietal in the final product. White blends vary in flavor, body, acidity, and alcohol content, but as a general guideline, they are most likely to pair with lighter dishes."
+      },
+      {
+        "Variety": "Tempranillo",
+        "Type": "Red",
+        "Descriptions": "Tempranillo is a black grape variety widely grown to make full-bodied red wines in its native Spain. It has a relatively neutral profile so it is often blended with other varieties or aged for extended periods in oak.  Varietal examples of Tempranillo usually exhibit flavors of plum and strawberries."
+      },
+      {
+        "Variety": "Rhône-style Red Blend",
+        "Type": "Red",
+        "Descriptions": "Rhône-style red blends are wines that are made in the style of the wines of France's Southern Rhône Valley. These red wines are typically made using Grenache, Syrah, Mourvèdre, Carignan and Cinsault, although other secondary grape varieties may be used, too."
+      },
+      {
+        "Variety": "Pinot Gris",
+        "Type": "White",
+        "Descriptions": "Pinot gris, pinot grigio or Grauburgunder is a white wine grape variety of the species Vitis vinifera.  it normally has a grayish-blue fruit, tending to have moderate to low acidity and higher alcohol levels. The flavors can range from ripe tropical fruit notes of melon and mango to some botrytis-influenced flavors."
+      },
+      {
+        "Variety": "Cabernet Franc",
+        "Type": "Red",
+        "Descriptions": "Cabernet Franc is medium-bodied red wine whose origins likely lie in the Basque country of France. The wine is loved for its savory, bell pepper-like flavors, medium-high acidity and mouthwatering taste. It is an ideal food pairing wine."
+      },
+      {
+        "Variety": "Grüner Veltliner",
+        "Type": "White",
+        "Descriptions": "The Grüner Veltliner grape is versatile and can produce a wide array of wines, from light and quaffable to rich and concentrated. The best dry Grüner Veltliners are perfumed, bone dry and full bodied, with high acidity and distinctive notes of spice and white pepper."
+      },
+      {
+        "Variety": "Portuguese White",
+        "Type": "White",
+        "Descriptions": "Portuguese producers make charming white wines from native grapes that are perfect for sunny afternoons. In the northern region of Vinho Verde, the variety Alvarinho flourishes alongside Loureiro. Both create crisp, and often lightly effervescent, wines."
+      },
+      {
+        "Variety": "Viognier",
+        "Type": "White",
+        "Descriptions": "Viognier (“Vee-own-yay”) is a full-bodied white wine that originated in southern France. Most loved for its perfumed aromas of peach, tangerine and honeysuckle, Viognier can also be oak-aged to add a rich creamy taste with hints of vanilla."
+      },
+      {
+        "Variety": "Gewürztraminer",
+        "Type": "White",
+        "Descriptions": "Gewürztraminer is a variety with a pink to red skin colour, which makes it a \"white wine grape\" as opposed to the blue to black-skinned varieties commonly referred to as \"red wine grapes\". The variety has high natural sugar and the wines are white and usually off-dry, with a flamboyant bouquet of lychees."
+      },
+      {
+        "Variety": "Gamay",
+        "Type": "Red",
+        "Descriptions": "Gamay is a light-bodied red wine that's similar in taste to Pinot Noir. Gamay wines are loved for their delicate floral aromas, subtle earthy notes, and surprising ability to pair with food."
+      },
+      {
+        "Variety": "Bordeaux-style White Blend",
+        "Type": "White",
+        "Descriptions": "The phrase “Bordeaux-style red blend” may be used informally to describe white wines,  often one that’s based on Cabernet Sauvignon or Merlot. Most Bordeaux-style blends use only the approved grapes of the Bordeaux region."
+      },
+      {
+        "Variety": "Pinot Grigio",
+        "Type": "White",
+        "Descriptions": "The Pinot grigio style of Italy is a light-bodied, often lean wine that is light in color with sometimes spritzy flavors that can be crisp and acidic."
+      },
+      {
+        "Variety": "Sparkling Blend",
+        "Type": "White",
+        "Descriptions": "Sparkling blend\" refers to sparkling wines that blend grapes of different varietals and vintages. Sparkling wine is only considered Champagne when grown in Champagne, France."
+      },
+      {
+        "Variety": "Petite Sirah",
+        "Type": "Red",
+        "Descriptions": "Petite Sirah produces inky dark, purple colored wines that offer flavors of black pepper, blackberries, blueberries, spice and licorice. They are often, massive, intense, chewy, masculine wines that when they are young, provide massive amounts of tannin and high levels of acidity."
+      },
+      {
+        "Variety": "Shiraz",
+        "Type": "Red",
+        "Descriptions": "The style and flavor profile of Shiraz, also known as Syrah, are influenced by the climate where the grapes are grown. It ranges from medium to full-bodied wines with notes of blackberry, mint and black pepper to a more full-bodied with softer tannin, jammier fruit and spice notes of licorice, anise and earthy leather."
+      },
+      {
+        "Variety": "Sangiovese Grosso",
+        "Type": "Red",
+        "Descriptions": "Sangiovese often has a flavour profile of sour red cherries with earthy aromas and tea leaf notes. Wines made from Sangiovese usually have medium-plus tannins and high acidity.[11]"
+      },
+      {
+        "Variety": "Barbera",
+        "Type": "Red",
+        "Descriptions": "Barbera is a red Italian wine grape variety that, as of 2000, was the third most-planted red grape variety in Italy (after Sangiovese and Montepulciano). It produces good yields and is known for deep color, full body, low tannins and high levels of acidity."
+      },
+      {
+        "Variety": "Corvina, Rondinella, Molinara",
+        "Type": "Red",
+        "Descriptions": "Corvina is used with several other grapes to create the light red regional wines Bardolino and Valpolicella that have a mild fruity flavor with hints of almond. These blends include Corvinone, Rondinella, and Molinara, and Rossignola for the latter wine. It is also used for the production of Amarone and Recioto."
+      },
+      {
+        "Variety": "Grenache",
+        "Type": "Red",
+        "Descriptions": "Grenache, also called Garnacha,  is generally spicy, berry-flavored and soft on the palate and produces wine with a relatively high alcohol content, but it needs careful control of yields for best results. Characteristic flavor profiles on Grenache include red fruit flavors (raspberry and strawberry) with a subtle, white pepper spice note."
+      },
+      {
+        "Variety": "Chenin Blanc",
+        "Type": "White",
+        "Descriptions": "A particularly identifiable grape based on aroma and palate, Chenin Blanc is often known for its notes of honey and straw. Wines are also often described as having a perceptible minerality, as well as a palate that is rich in stone-fruit flavors."
+      },
+      {
+        "Variety": "Tempranillo Blend",
+        "Type": "Red",
+        "Descriptions": "Tempranillo wines are ruby red in colour, while aromas and flavours can include berries, plum, tobacco, vanilla, leather and herb. Often making up as much as 90% of a blend, Tempranillo is less frequently bottled as a single varietal."
+      },
+      {
+        "Variety": "Carmenère",
+        "Type": "Red",
+        "Descriptions": "Carménère (“car-men-nair”) is a medium-bodied red wine that originated in Bordeaux, France, and now grows almost only in Chile. The wine is treasured for its supple red-and-black berry flavors (in a similar style to Merlot) and herbaceous green peppercorn notes."
+      },
+      {
+        "Variety": "Champagne Blend",
+        "Type": "White",
+        "Descriptions": "The term Champagne Blend refers to one of the world's most distinctive wine styles – the sparkling wines made from Pinot Noir, Chardonnay and Pinot Meunier that are most famously associated with the Champagne region of northern France."
+      },
+      {
+        "Variety": "Albariño",
+        "Type": "White",
+        "Descriptions": "Albariño wine is a delightfully refreshing coastal white that grows on the Iberian Peninsula. It's loved for its rich stone fruit flavors, a hint of salinity, and zippy acidity."
+      },
+      {
+        "Variety": "Port",
+        "Type": "Red",
+        "Descriptions": "Port is a sweet, red, fortified wine from Portugal. Port wine is most commonly enjoyed as a dessert wine because its richness."
+      },
+      {
+        "Variety": "Rhône-style White Blend",
+        "Type": "White",
+        "Descriptions": "Rhône-style white blends are wines that are made in the style of the wines of France's Southern Rhône Valley."
+      },
+      {
+        "Variety": "Pinot Blanc",
+        "Type": "White",
+        "Descriptions": "Pinot blanc is a white wine grape. It is a point genetic mutation of Pinot noir. Bottles labeled Pinot Blanc offer fruity aromas, often of apple, citrus fruit, and floral characteristics. It is rather high in acidity and usually made for immediate consumption."
+      },
+      {
+        "Variety": "Aglianico",
+        "Type": "Red",
+        "Descriptions": "Aglianico is a full-bodied red wine which is found almost exclusively in Southern Italy. Young Aglianico wines are known for strikingly savory flavors of leather, white pepper, black fruits and cured meat that when aged, develop soft dusty aromas of dried figs and sun-tanned leather."
+      },
+      {
+        "Variety": "Garganega",
+        "Type": "White",
+        "Descriptions": "Garganega is an Italian grape used to make white wine in the region of Soave. It produces dry, light- to medium-bodied wines that exude flavors and aromas of peach, honeydew, melon, tangerine, almond, and herbs such as marjoram."
+      },
+      {
+        "Variety": "Moscato",
+        "Type": "White",
+        "Descriptions": "Moscato is a style of wine made from muscat grapes. It's famous for sweet flavours of peaches and orange blossom and because it's lower in alcohol than other sparkling wines."
+      },
+      {
+        "Variety": "Nero d'Avola",
+        "Type": "Red",
+        "Descriptions": "Nero d'Avola is \"the most important red wine grape in Sicily\" and is one of Italy's most important indigenous varieties. It is named after Avola in the far south of Sicily, and its wines are compared to New World Shirazes, with sweet tannins and plum or peppery flavours."
+      },
+      {
+        "Variety": "Glera",
+        "Type": "White",
+        "Descriptions": "Glera is a rather neutral grape variety which is mainly cultivated for use in sparkling Italian wine styles, frizzante or spumante, from the various Prosecco DOCG and DOC areas, although still wines also exist."
+      },
+      {
+        "Variety": "Garnacha",
+        "Type": "Red",
+        "Descriptions": "Garnacha, also called Grenache, is generally spicy, berry-flavored and soft on the palate and produces wine with a relatively high alcohol content, but it needs careful control of yields for best results. Characteristic flavor profiles on Grenache include red fruit flavors (raspberry and strawberry) with a subtle, white pepper spice note."
+      },
+      {
+        "Variety": "Petit Verdot",
+        "Type": "Red",
+        "Descriptions": "Petit Verdot is a variety of red wine grape, principally used in classic Bordeaux blends. It ripens much later than the other varieties in Bordeaux, often too late, so it fell out of favour in its home region. When it does ripen it adds tannin, colour and flavour, in small amounts, to the blend."
+      },
+      {
+        "Variety": "Sauvignon",
+        "Type": "White",
+        "Descriptions": "Sauvignon Blanc is a white wine grape variety that is most often used to produce dry white wines. The flavor can range from aggressively grassy to sweetly tropical."
+      }
+]
