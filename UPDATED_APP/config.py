c4c3e273415b57f87387ab2f417c05b9e289c268
EmmaAB <66276192+EmmaAB@users.noreply.github.com>
2020-10-19 20:42:58 -0700

    Update config.py

--- UPDATED_APP/config.py
+++ UPDATED_APP/config.py
@@ -1,3 +1,4 @@
-db_username = "postgres"
-# db_password = "Hyunjung25$"
+##Input your username and password. Update the postgres port if it varies from the default 5432
+db_username = "postgres_username"
+db_password = "postgres_password"
 postgres_port = "5432"
